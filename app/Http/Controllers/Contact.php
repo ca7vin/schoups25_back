6ab39757539bb7238658e5eb10dcd35c4ff00efa
ca7vin <ca7vin@gmail.com>
2025-04-15 15:06:24 +0200

    Mail Config

--- app/Http/Controllers/Contact.php
+++ app/Http/Controllers/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactFormMail;
 use App\Settings\EmailSettings;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -11,6 +12,13 @@ class Contact extends Controller
     public function sendEmail(Request $request, EmailSettings $emailContact)
     {
         // Validez les données du formulaire ici si nécessaire
+        $data = $request->validate([
+            'name' => 'required|string',
+            'subject' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+
         $contactMail = $emailContact->contact;
         // Envoyez l'e-mail
         Mail::to($contactMail)->send(new ContactFormMail($request->all()));
